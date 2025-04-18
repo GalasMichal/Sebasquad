efea263215c76ef4e982d285698f3ef0c978df9b
Mike <michalgalas1989@gmail.com>
2025-04-18 20:35:29 +0200

    files and components added , simple routing for begining with lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, // Lazy Loading von Home
+    { path: 'shop', loadChildren: () => import('./shop/shop.module').then(m => m.ShopModule) }, // Lazy Loading für Shop
+    { path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule) }, // Lazy Loading für Contact
+  ];
+
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+
+  export class AppRoutingModule { }
